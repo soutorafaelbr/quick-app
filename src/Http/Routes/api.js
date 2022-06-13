04a507d705387a29e8893380edeb12ea9ba4d406
Souto <rafael.souto@melhorenvio.com>
2022-06-13 19:05:25 -0300

    chore: add reset route

--- src/Http/Routes/api.js
+++ src/Http/Routes/api.js
@@ -3,8 +3,10 @@ const express = require('express');
 const routes = express.Router();
 const eventController = require('../Controllers/event.controller');
 const balanceController = require('../Controllers/balance.controller');
+const resetController = require('../Controllers/reset.controller');
 
 routes.get('/event', eventController.get);
 routes.get('/balance', balanceController.get);
+routes.get('/reset', resetController.get);
 
 module.exports = routes;
