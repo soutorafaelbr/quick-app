a2a505bfbb793895b7286810eee394cd17bbc12e
Souto <rafael.souto@melhorenvio.com>
2022-06-14 12:53:27 -0300

    refactor: reset controller test

--- src/Http/Controllers/reset.controller.test.js
+++ src/Http/Controllers/reset.controller.test.js
@@ -1,12 +1,14 @@
-import app from '../../../index.js';
+import app from '../../../server';
 import supertest from 'supertest';
 import statuses from "../../App/Enums/HttpStatusesEnum.js";
 
 const request = supertest(app);
 
-describe('reset controller', () => {
-    test('given valid http request when /reset is requested should responds with http 200 OK', async () => {
-        const response = await request.get('/reset');
-        expect(response.statusCode).toBe(statuses.HTTP_OK);
+describe('given valid http request when /reset is requested ', () => {
+    it('should responds with http 200 OK', () => {
+        return request
+            .get('/reset')
+            .then(response => expect(response.statusCode)
+            .toBe(statuses.HTTP_OK));
     });
 });
