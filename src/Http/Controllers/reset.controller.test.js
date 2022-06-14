715a18582e8631a59547665fb2c088e59ddc4a69
Souto <rafael.souto@melhorenvio.com>
2022-06-14 11:07:24 -0300

    refactor: change static http status to enum

--- src/Http/Controllers/reset.controller.test.js
+++ src/Http/Controllers/reset.controller.test.js
@@ -1,11 +1,12 @@
 import app from '../../../index.js';
 import supertest from 'supertest';
+import statuses from "../../App/Enums/HttpStatusesEnum.js";
 
 const request = supertest(app);
 
 describe('reset controller', () => {
     test('given valid http request when /reset is requested should responds with http 200 OK', async () => {
         const response = await request.get('/reset');
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(statuses.HTTP_OK);
     });
 });
